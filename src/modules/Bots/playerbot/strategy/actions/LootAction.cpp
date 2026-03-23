58153fedb60b5472833350d27acefab04261831f
bo <bo@zimmers.net>
2026-03-22 20:42:50 -0500

    Bot loot fixes: rogue chest multi-player looting, loot roll packet, dungeon gathering

--- src/modules/Bots/playerbot/strategy/actions/LootAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/LootAction.cpp
@@ -103,6 +103,23 @@ bool OpenLootAction::DoLoot(LootObject& lootObject)
     }
 
     bot->GetMotionMaster()->Clear();
+
+    if (go && go->GetGoState() == GO_STATE_ACTIVE)
+    {
+        if (bot->GetLootGuid() == lootObject.guid)
+            return false;
+
+        WorldPacket* const packet = new WorldPacket(CMSG_LOOT, 8);
+        *packet << lootObject.guid;
+        bot->GetSession()->QueuePacket(packet);
+        return true;
+    }
+
+    if (bot->IsNonMeleeSpellCasted(false))
+    {
+        return false;
+    }
+
     if (lootObject.skillId == SKILL_MINING)
     {
         return bot->HasSkill(SKILL_MINING) ? ai->CastSpell(MINING, bot) : false;
@@ -119,7 +136,8 @@ bool OpenLootAction::DoLoot(LootObject& lootObject)
         return false;
     }
 
-    return ai->CastSpell(spellId, bot);
+    ai->CastSpell(spellId, bot);
+    return false;
 }
 
 uint32 OpenLootAction::GetOpeningSpell(LootObject& lootObject)
