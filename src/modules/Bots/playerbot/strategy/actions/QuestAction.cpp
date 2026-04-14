5b251a8603691aa82fb5f4b88384e54f016b2074
bo <bo@zimmers.net>
2026-04-13 19:58:53 -0500

    bots dont complain about vendors

--- src/modules/Bots/playerbot/strategy/actions/QuestAction.cpp
+++ src/modules/Bots/playerbot/strategy/actions/QuestAction.cpp
@@ -54,6 +54,8 @@ bool QuestAction::ProcessQuests(ObjectGuid questGiver)
     Creature* creature = ai->GetCreature(questGiver);
     if (creature)
     {
+        if (!creature->isQuestGiver())
+            return false;
         return ProcessQuests(creature);
     }
 
