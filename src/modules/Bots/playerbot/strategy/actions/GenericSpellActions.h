19dfd55d927ddfc97556a63c05fe43a38230fbfc
bo <bo@zimmers.net>
2026-05-11 10:55:46 -0500

    bot remote pull ability

--- src/modules/Bots/playerbot/strategy/actions/GenericSpellActions.h
+++ src/modules/Bots/playerbot/strategy/actions/GenericSpellActions.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Action.h"
+#include "../AiObjectContext.h"
 #include "../../PlayerbotAIConfig.h"
 
 #define BEGIN_SPELL_ACTION(clazz, name) \
@@ -52,21 +53,7 @@ namespace ai
         {
             this->spell = spell;
             // Clamp range to actual spell's min/max range from DBC
-            uint32 spellId = AI_VALUE2(uint32, "spell id", spell);
-            if (spellId)
-            {
-                const SpellEntry* pSpellInfo = sSpellStore.LookupEntry(spellId);
-                if (pSpellInfo)
-                {
-                    SpellRangeEntry const* spellRange = sSpellRangeStore.LookupEntry(pSpellInfo->rangeIndex);
-                    if (spellRange)
-                    {
-                        float actualMaxRange = GetSpellMaxRange(spellRange);
-                        if (actualMaxRange > 0)  // Only clamp if spell has a defined range
-                            range = actualMaxRange;
-                    }
-                }
-            }
+            range = AI_VALUE2(float, "spell range", spell);
         }
 
         virtual string GetTargetName() { return "current target"; };
@@ -260,13 +247,6 @@ namespace ai
     };
 
     //---------------------------------------------------------------------------------------------------------------------
-
-    class CastShootAction : public CastSpellAction
-    {
-    public:
-        CastShootAction(PlayerbotAI* ai) : CastSpellAction(ai, "shoot") {}
-        virtual ActionThreatType getThreatType() { return ACTION_THREAT_NONE; }
-    };
 
     class CastLifeBloodAction : public CastHealingSpellAction
     {
